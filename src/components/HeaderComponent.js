35ac7e1f4d8a3c14c33c72dec3992ba6bf57ae4e
Vincent <vincent930093@gmail.com>
2021-08-15 16:41:02 +0800

    make login button be in the right

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -80,12 +80,13 @@ class Header extends Component {
                                 <NavLink className="nav-link" to='/'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
                             </NavItem>
                         </Nav>
-                        <Nav className="ml-auto" navbar>
+                        {/* <Nav className="ml-auto" navbar>
                             <NavItem>
                                 <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
                             </NavItem>
-                        </Nav>
+                        </Nav> */}
                     </Collapse>
+                    <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
                 </div>
             </Navbar>
             <Jumbotron>
